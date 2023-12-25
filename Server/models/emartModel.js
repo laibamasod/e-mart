423ee3e1516886ca3d95b57c5535d8d8ac1cb3d1
laibamasod <laibamasoodd@gmail.com>
2023-12-26 00:16:45 +0500

    db connected

---
+++ Server/models/emartModel.js
@@ -0,0 +1,114 @@
+const mongoose = require('mongoose');
+
+// Schema for Products
+const productSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  rating: Number,
+  productQuantity: Number,
+  salesQuantity: Number,
+  categoryID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category'
+  },
+  sellerID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Seller'
+  }
+});
+
+const Product = mongoose.model('Product', productSchema);
+
+// Schema for Category
+const categorySchema = new mongoose.Schema({
+  name: String
+});
+
+const Category = mongoose.model('Category', categorySchema);
+
+// Schema for Buyer
+const buyerSchema = new mongoose.Schema({
+  buyerRegID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'BuyerRegistration'
+  },
+  shippingAddress: String,
+  contact: String
+});
+
+const Buyer = mongoose.model('Buyer', buyerSchema);
+
+// Schema for Buyer Registration
+const buyerRegistrationSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+  verificationCode: String
+});
+
+const BuyerRegistration = mongoose.model('BuyerRegistration', buyerRegistrationSchema);
+
+// Schema for Purchase
+const purchaseSchema = new mongoose.Schema({
+  productID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product'
+  },
+  buyerID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Buyer'
+  },
+  date: Date,
+  purchaseQuantity: Number,
+  purchasePrice: Number
+});
+
+const Purchase = mongoose.model('Purchase', purchaseSchema);
+
+// Schema for Cart
+const cartSchema = new mongoose.Schema({
+  productID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Product'
+  },
+  buyerID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Buyer'
+  }
+});
+
+const Cart = mongoose.model('Cart', cartSchema);
+
+// Schema for Seller Registration
+const sellerRegistrationSchema = new mongoose.Schema({
+  company_name: String,
+  email: String,
+  password: String,
+  verificationCode: String
+});
+
+const SellerRegistration = mongoose.model('SellerRegistration', sellerRegistrationSchema);
+
+// Schema for Seller
+const sellerSchema = new mongoose.Schema({
+  sellerRegID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'SellerRegistration'
+  },
+  profilePicture: String,
+  headline: String
+});
+
+const Seller = mongoose.model('Seller', sellerSchema);
+
+module.exports = {
+  Product,
+  Category,
+  Buyer,
+  BuyerRegistration,
+  Purchase,
+  Cart,
+  SellerRegistration,
+  Seller
+};
