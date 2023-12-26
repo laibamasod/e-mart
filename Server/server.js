c78599691f0f997b8dfcc85713b468d890063077
Nasir Hussain <nasirhussaintormik@gmail.com>
2023-12-26 23:58:16 +0500

    product controller completed CRUD

--- Server/server.js
+++ Server/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
 require('./models/db');
-const emartController = require('./controllers/emartController');
+const productController = require('./controllers/productController');
 const bodyParser = require("body-parser");
 
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
-app.use("/", emartController);
+app.use("/product", productController );
 app.listen(8080, ()=>{
     console.log("Server at 8080");
 });
