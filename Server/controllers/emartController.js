cd20d377a050da35c684d594f6304b1d291cabcb
laibamasod <laibamasoodd@gmail.com>
2023-12-26 21:17:51 +0500

    controller defined

---
+++ Server/controllers/emartController.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const {
+    Product,
+    Category,
+    Buyer,
+    BuyerRegistration,
+    Purchase,
+    Cart,
+    SellerRegistration,
+    Seller
+  } = require('../models/emartModel')
+const router = express.Router();
+
+
+
--- Server/server.js
+++ Server/server.js
@@ -1,8 +1,12 @@
 const express = require('express');
 require('./models/db');
+const emartController = require('./controllers/emartController');
+const bodyParser = require("body-parser");
 
 const app = express();
-
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use("/", emartController);
 app.listen(8080, ()=>{
     console.log("Server at 8080");
 });
