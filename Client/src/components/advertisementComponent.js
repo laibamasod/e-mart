c52724c327496cba6009aac26330e164317c1eaf
laibamasod <laibamasoodd@gmail.com>
2024-01-03 20:28:38 +0500

    check ad section

--- Client/src/components/advertisementComponent.js
+++ Client/src/components/advertisementComponent.js
@@ -9,7 +9,7 @@ import adPic7 from "../images/adpic7.jpg";
 import adPic8 from "../images/adpic8.jpg";
 import adPic9 from "../images/adpic9.jpg";
 import adPic10 from "../images/adpic10.jpg";
-
+import './style/advertisement.css';
 const images = [
   adPic1,
   adPic2,
@@ -21,17 +21,16 @@ const images = [
   adPic8,
   adPic9,
   adPic10,
-]; // Add other images to this array
+];
 
 const AdvertisementComponent = () => {
   const numberOfAds = images.length;
   const [currentAdIndex, setCurrentAdIndex] = useState(0);
 
-  // Function to switch to the next advertisement after a certain interval (5 seconds)
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentAdIndex((prevIndex) => (prevIndex + 1) % numberOfAds);
-    }, 5000); // Change the interval time in milliseconds as needed
+    }, 5000);
 
     return () => clearInterval(interval);
   }, [numberOfAds]);
@@ -47,8 +46,48 @@ const AdvertisementComponent = () => {
   };
 
   return (
-    <div className="container">
+    <div className="container mt-3">
       <div className="row">
+        <div className="col-md-2">
+          <nav className="navbar navbar1 bg-light ">
+            <div className="container-fluid">
+              <ul className="navbar-nav cat-nav navhead">
+                <li className="nav-item navitems">
+                  <a className="nav-link nav1-link" href="#">
+                    Groceries
+                  </a>
+                </li>
+                <li class="nav-item navitems">
+        <a class="nav-link nav1-link" href="#">Health and Beauty</a>
+      </li>
+      <li class="nav-item navitems">
+        <a class="nav-link nav1-link" href="#">Men's Fashion</a>
+      </li>
+      <li class="nav-item navitems">
+        <a class="nav-link nav1-link" href="#">Women's Fashion</a>
+      </li>
+      <li class="nav-item navitems">
+        <a class="nav-link nav1-link" href="#">Electronic Devices</a>
+      </li>
+      <li class="nav-item navitems">
+        <a class="nav-link nav1-link" href="#">Sports and OutDoor</a>
+      </li>
+      <li class="nav-item navitems">
+        <a class="nav-link nav1-link" href="#">Jewellery and Accessories</a>
+      </li>
+      <li class="nav-item navitems">
+        <a class="nav-link nav1-link" href="#">TV and Home Appliances</a>
+      </li>
+      <li class="nav-item navitems">
+        <a class="nav-link nav1-link" href="#">Motorbike and Automobiles</a>
+      </li>
+      <li class="nav-item navitems">
+        <a class="nav-link nav1-link" href="#">Pets</a>
+      </li>
+              </ul>
+            </div>
+          </nav>
+        </div>
         <div className="col-md-10">
           <div className="card">
             <div className="card-body p-0 position-relative">
@@ -58,28 +97,6 @@ const AdvertisementComponent = () => {
                 data-bs-ride="carousel"
                 style={{ position: "relative" }}
               >
-                <button
-                  className="carousel-control-prev"
-                  type="button"
-                  onClick={prevSlide}
-                >
-                  <span
-                    className="carousel-control-prev-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Previous</span>
-                </button>
-                <button
-                  className="carousel-control-next"
-                  type="button"
-                  onClick={nextSlide}
-                >
-                  <span
-                    className="carousel-control-next-icon"
-                    aria-hidden="true"
-                  ></span>
-                  <span className="visually-hidden">Next</span>
-                </button>
                 <div className="carousel-inner">
                   {images.map((image, index) => (
                     <div
@@ -89,46 +106,51 @@ const AdvertisementComponent = () => {
                       }`}
                     >
                       <div
-                        className="card"
+                        className="card ad-card"
                         style={{
                           backgroundImage: `url(${image})`,
+                          height: "389px",
                           backgroundSize: "cover",
-                          height: "350px",
-                          width: "950px", // Adjust the height as needed
-                          position: "relative",
                         }}
-                      >
-                      </div>
+                      ></div>
                     </div>
                   ))}
-                  <div className="ad-dots mt-3 d-flex justify-content-center position-absolute bottom-0 w-100">
-                    {[...Array(numberOfAds)].map((_, index) => (
-                      <span
-                        key={index}
-                        onClick={() => setCurrentAdIndex(index)}
-                        className={`dot${
-                          index === currentAdIndex ? " active" : ""
-                        }`}
-                        style={{
-                          cursor: "pointer",
-                          margin: "0 5px",
-                          display: "inline-block",
-                          width: "12px",
-                          height: "12px",
-                          borderRadius: "50%",
-                          backgroundColor:
-                            index === currentAdIndex
-                              ? "transparent"
-                              : "transparent",
-                          border:
-                            index === currentAdIndex
-                              ? "2px solid #000"
-                              : "2px solid #ccc",
-                        }}
-                      ></span>
-                    ))}
-                  </div>
                 </div>
+                <div className="ad-dots mt-3 d-flex justify-content-center position-absolute bottom-0 w-100">
+                  {[...Array(numberOfAds)].map((_, index) => (
+                    <span
+                      key={index}
+                      onClick={() => setCurrentAdIndex(index)}
+                      className={`dot${
+                        index === currentAdIndex ? " active" : ""
+                      }`}
+                      style={{
+                        cursor: "pointer",
+                        margin: "0 5px",
+                        display: "inline-block",
+                        width: "12px",
+                        height: "12px",
+                        borderRadius: "50%",
+                        backgroundColor:
+                          index === currentAdIndex ? "#000" : "#ccc",
+                      }}
+                    ></span>
+                  ))}
+                </div>
+                <button
+                  className="carousel-control-prev"
+                  type="button"
+                  onClick={prevSlide}
+                >
+                  {/* Previous icon */}
+                </button>
+                <button
+                  className="carousel-control-next"
+                  type="button"
+                  onClick={nextSlide}
+                >
+                  {/* Next icon */}
+                </button>
               </div>
             </div>
           </div>
